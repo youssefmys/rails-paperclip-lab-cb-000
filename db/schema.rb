c7efa8e01723da1f91b7ae913addcf9952ec9f83
youssefmys <mosa500@gmail.com>
2018-04-06 17:38:15 +0200

    Done

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126235547) do
+ActiveRecord::Schema.define(version: 20180406145642) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -21,9 +21,13 @@ ActiveRecord::Schema.define(version: 20160126235547) do
 
   create_table "songs", force: :cascade do |t|
     t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
     t.integer  "artist_id"
+    t.string   "album_cover_file_name"
+    t.string   "album_cover_content_type"
+    t.integer  "album_cover_file_size"
+    t.datetime "album_cover_updated_at"
   end
 
   add_index "songs", ["artist_id"], name: "index_songs_on_artist_id"
